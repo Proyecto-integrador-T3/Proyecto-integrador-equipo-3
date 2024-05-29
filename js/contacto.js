a74b0d7be221fff5ff1dd81f13a9819e8cd0cebe
Sergio Sanchez <sergiosm548@gmail.com>
2024-05-29 01:50:38 -0600

    Cambios en el formulario de contacto, se añadio api de webforms

--- js/contacto.js
+++ js/contacto.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-        if (validateForm()) {
-            alert('Formulario enviado con éxito.');
+        if (validateForm() && validateCaptcha()) {
             form.submit();
+            form.reset(); // Restablecer el formulario después del envío
         }
     });
 
@@ -13,52 +13,62 @@ document.addEventListener('DOMContentLoaded', function() {
         let isValid = true;
 
         // Validar el nombre
-        const name = document.getElementById('exampleInputName');
+        const name = document.querySelector('[name="fullName"]');
         if (name.value.trim() === '') {
             name.classList.add('is-invalid');
+            document.getElementById('fullNameAlert').classList.remove('d-none');
             isValid = false;
         } else {
             name.classList.remove('is-invalid');
-        }
-
-        // Validar los apellidos
-        const lastName = document.getElementById('exampleInputLastName');
-        if (lastName.value.trim() === '') {
-            lastName.classList.add('is-invalid');
-            isValid = false;
-        } else {
-            lastName.classList.remove('is-invalid');
-        }
-
-        // Validar el correo electrónico
-        const email = document.getElementById('exampleInputEmail1');
-        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-        if (!emailPattern.test(email.value.trim())) {
-            email.classList.add('is-invalid');
-            isValid = false;
-        } else {
-            email.classList.remove('is-invalid');
+            document.getElementById('fullNameAlert').classList.add('d-none');
         }
 
         // Validar el teléfono
-        const phone = document.getElementById('exampleInputPhone');
+        const phone = document.querySelector('[name="cellNumber"]');
         const phonePattern = /^[0-9]{10}$/;
         if (!phonePattern.test(phone.value.trim())) {
             phone.classList.add('is-invalid');
+            document.getElementById('cellNumberAlert').classList.remove('d-none');
             isValid = false;
         } else {
             phone.classList.remove('is-invalid');
+            document.getElementById('cellNumberAlert').classList.add('d-none');
+        }
+
+        // Validar el correo electrónico
+        const email = document.querySelector('[name="email"]');
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if (!emailPattern.test(email.value.trim())) {
+            email.classList.add('is-invalid');
+            document.getElementById('emailAlert').classList.remove('d-none');
+            isValid = false;
+        } else {
+            email.classList.remove('is-invalid');
+            document.getElementById('emailAlert').classList.add('d-none');
         }
 
         // Validar comentarios
-        const comments = document.getElementById('floatingTextarea');
+        const comments = document.querySelector('[name="comments"]');
         if (comments.value.trim() === '') {
             comments.classList.add('is-invalid');
+            document.getElementById('commentsAlert').classList.remove('d-none');
             isValid = false;
         } else {
             comments.classList.remove('is-invalid');
+            document.getElementById('commentsAlert').classList.add('d-none');
         }
 
         return isValid;
+    }
+
+    function validateCaptcha() {
+        const captchaResponse = document.querySelector('[name="h-captcha-response"]').value;
+        if (captchaResponse === '') {
+            document.getElementById('captchaAlert').classList.remove('d-none');
+            return false;
+        } else {
+            document.getElementById('captchaAlert').classList.add('d-none');
+            return true;
+        }
     }
 });
